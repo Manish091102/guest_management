23349bf02c5917de59e059e7fedf635b7875d605
manish091102 <manish091102@gmail.com>
2025-04-02 18:35:45 +0530

    Build

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Ensure correct paths for production
   build: {
-    outDir: "dist"
+    outDir: "dist",
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': '/src',  // No need for 'path.resolve' here in Vite
     },
   },
 });
